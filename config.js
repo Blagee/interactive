ca6174ce6d57c97be70f6c0f37179dcd7f8b3e1c
Blagee <77645885+Blagee@users.noreply.github.com>
2021-03-23 03:20:47 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -5,20 +5,20 @@ var config = {
     markerColor: '#3FB1CE',
     theme: 'light',
     use3dTerrain: false,
-    title: 'Experience the Stanley Park Seawall',
+    title: 'A Ride Around Stanley Park',
     subtitle: 'Take a ride around this world famous park',
     byline: 'By Blagee Zhuo',
     footer: 'Please remember to be respectful when cycling and always wear a helmet!',
     chapters: [
         {
             id: 'stanleypark',
-            alignment: 'left',
+            alignment: 'right',
             hidden: false,
             title: 'Welcome to Stanley Park',
             image: 'https://vancouver.ca/images/cov/feature/stanley-park-entrace-landing.jpg',
             description: 'Stanley Park is the largest park in Vancouver and provides some of the most scenic opportunities in the lower mainland.',
             location: {
-                center: [-123.11707, 49.26124],
+                center: [-123.11707, 49.28338],
                 zoom: 11.76,
                 pitch: 20.00,
                 bearing: 0
@@ -29,13 +29,178 @@ var config = {
                      layer: 'stanleypark',
                      opacity: 1,
                  }
+                 {
+                      layer: 'seawall',
+                      opacity: 0,
+                  }
             ],
             onChapterExit: [
-                 {
+              {
                    layer: 'stanleypark',
                    opacity: 0,
-                 }
+               }
             ]
+        },
+        {
+            id: 'seawall',
+            alignment: 'left',
+            hidden: false,
+            title: 'The Seawall',
+            image: 'https://images.dailyhive.com/20200410150010/biking-stanley-park-seawall.jpg',
+            description: 'The seawall is a 10km path that wraps around Stanley Park and offers a refreshing way to experience both land and sea.',
+            location: {
+                center: [-123.14278, 49.30072],
+                zoom: 13.86,
+                pitch: 39.50,
+                bearing: 41.60
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            onChapterEnter: [
+              {
+                   layer: 'seawall',
+                   opacity: 1,
+               }
+            ],
+            onChapterExit: []
+        },
+        {
+            id: 'start',
+            alignment: 'right',
+            hidden: false,
+            title: 'Start Line',
+            image: 'http://ihikebc.com/trips/picsnew/2011/3744-View-From-Vancouver-Seawall-BC-Canada.JPG',
+            description: 'Begin your ride here at the bottom of Denman Street with your own bike, or with one you rented from any of the abundant bike shops nearby.',
+            location: {
+                center: [-123.13247, 49.29357],
+                zoom: 16.99,
+                pitch: 53.50,
+                bearing: 1.49
+            },
+            mapAnimation: 'flyTo',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'cannon',
+            alignment: 'left',
+            hidden: false,
+            title: "The Nine O'Clock Gun",
+            image: 'https://www.vmcdn.ca/f/files/via/images/stanley-park-cannon.jpg;w=960',
+            description: 'This historic naval cannon was cast in 1816 and brough to Stanley Park in 1894. It fires every evening at precisely 9pm.',
+            location: {
+                center: [-123.14278, 49.30072],
+                zoom: 17.14,
+                pitch: 47,
+                bearing: -71.30
+            },
+            mapAnimation: 'flyTo',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'lighthouse',
+            alignment: 'right',
+            hidden: false,
+            title: 'Brockton Point Lighthouse',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Brockton_Point_Light_and_Vancouver.jpg/1280px-Brockton_Point_Light_and_Vancouver.jpg',
+            description: 'Constructed in 1914, this lighthouse is a popular photo stop that looks across the Vancouver Harbour towards the North Shore.',
+            location: {
+                center: [-123.11710, 49.30077],
+                zoom: 15.78,
+                pitch: 58,
+                bearing: -118.50
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'siwash',
+            alignment: 'left',
+            hidden: false,
+            title: 'The Seawall',
+            image: 'https://images.dailyhive.com/20200410150010/biking-stanley-park-seawall.jpg',
+            description: 'The seawall is a 10km path that wraps around Stanley Park and offers a refreshing way to experience both land and sea.',
+            location: {
+                center: [-123.14278, 49.30072],
+                zoom: 13.86,
+                pitch: 39.50,
+                bearing: 41.60
+            },
+            mapAnimation: 'flyTo',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'thirdbeach',
+            alignment: 'right',
+            hidden: false,
+            title: 'The Seawall',
+            image: 'https://images.dailyhive.com/20200410150010/biking-stanley-park-seawall.jpg',
+            description: 'The seawall is a 10km path that wraps around Stanley Park and offers a refreshing way to experience both land and sea.',
+            location: {
+                center: [-123.14278, 49.30072],
+                zoom: 13.86,
+                pitch: 39.50,
+                bearing: 41.60
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            onChapterEnter: [
+              {
+                   layer: 'thirdbeach',
+                   opacity: 1,
+               }
+            ],
+            onChapterExit: []
+        },
+        {
+            id: 'secondbeach',
+            alignment: 'left',
+            hidden: false,
+            title: 'The Seawall',
+            image: 'https://images.dailyhive.com/20200410150010/biking-stanley-park-seawall.jpg',
+            description: 'The seawall is a 10km path that wraps around Stanley Park and offers a refreshing way to experience both land and sea.',
+            location: {
+                center: [-123.14278, 49.30072],
+                zoom: 13.86,
+                pitch: 39.50,
+                bearing: 41.60
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            onChapterEnter: [
+              {
+                   layer: 'seawall',
+                   opacity: 1,
+               }
+            ],
+            onChapterExit: []
+        },
+        {
+            id: 'lostlagoon',
+            alignment: 'right',
+            hidden: false,
+            title: 'The Seawall',
+            image: 'https://images.dailyhive.com/20200410150010/biking-stanley-park-seawall.jpg',
+            description: 'The seawall is a 10km path that wraps around Stanley Park and offers a refreshing way to experience both land and sea.',
+            location: {
+                center: [-123.14278, 49.30072],
+                zoom: 13.86,
+                pitch: 39.50,
+                bearing: 41.60
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            onChapterEnter: [
+              {
+                   layer: 'seawall',
+                   opacity: 1,
+               }
+            ],
+            onChapterExit: []
         },
         {
             id: 'seawall',
